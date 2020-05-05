641e8d7d8774af812519d2fc50ee107a53b229f0
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-05-05 17:24:45 +0530

    Add verbose loggers in task controller!

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
   UsePipes,
   ValidationPipe,
   UseGuards,
+  Logger,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { TasksService } from "./tasks.service";
@@ -24,6 +25,8 @@ import { ReadUser } from "../auth/read-user.decorator";
 @Controller("/tasks")
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger("TasksController");
+
   constructor(private tasksService: TasksService) {}
 
   @Post()
@@ -32,6 +35,11 @@ export class TasksController {
     @Body() newTaskDto: CreateNewTaskDto,
     @ReadUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `Creating new tasks for the User ${
+        user.username
+      }. Created Data: ${JSON.stringify(newTaskDto)}`,
+    );
     return this.tasksService.createNewTask(newTaskDto, user);
   }
 
@@ -40,11 +48,19 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: ReadTasksFilterDto,
     @ReadUser() user: User,
   ) {
+    this.logger.verbose(
+      `Reading tasks for the User ${
+        user.username
+      } with filters: ${JSON.stringify(filterDto)}.`,
+    );
     return this.tasksService.readTasks(filterDto, user);
   }
 
   @Get("/:id")
   readTaskById(@Param("id") id: string, @ReadUser() user: User): Promise<Task> {
+    this.logger.verbose(
+      `Reading task for the User ${user.username} with id: ${id}.`,
+    );
     return this.tasksService.readTaskById(id, user);
   }
 
@@ -54,6 +70,9 @@ export class TasksController {
     @Body("status", TaskStatusValidationPipe) status: TaskStatus,
     @ReadUser() user: User,
   ): Promise<Task> {
+    this.logger.verbose(
+      `Updating task status for the User ${user.username} with id: ${id}. Updated Status: ${status}`,
+    );
     return this.tasksService.updateTaskStatusById(id, status, user);
   }
 
@@ -62,6 +81,9 @@ export class TasksController {
     @Param("id") id: string,
     @ReadUser() user: User,
   ): Promise<void> {
+    this.logger.verbose(
+      `Deleting task status for the User ${user.username} with id: ${id}.`,
+    );
     return this.tasksService.deleteTaskById(id, user);
   }
 }
