939b7163a98cc922475705d17a7f25bcd147eb55
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-05-05 18:57:56 +0530

    Add debug log for payload generation!

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { JwtService } from "@nestjs/jwt";
 import { UserRepository } from "./user.repository";
@@ -8,6 +8,8 @@ import { JwtAccessToken } from "./jwt-access-token-interface";
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger("AuthService");
+
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
     private jwtService: JwtService,
@@ -26,6 +28,10 @@ export class AuthService {
     }
     const payload: JwtPayload = { username };
     const accessToken = await this.jwtService.sign(payload);
+
+    this.logger.debug(
+      `Generated JWT token with payload: ${JSON.stringify(payload)}`,
+    );
     return { accessToken };
   }
 }
