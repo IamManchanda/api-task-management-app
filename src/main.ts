23fbb1672b4ebc87c79c0cfc1694c819a15d42ec
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-05-07 15:48:53 +0530

    Enabling CORS for the Web Front-end Application

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
   const serverConfig = config.get("server");
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin ${serverConfig.origin}`);
+  }
   const port = parseInt(process.env.PORT, 10) || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
