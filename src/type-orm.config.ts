159dcc967d7a87a46ba554437fe50521078405c7
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-05-07 19:04:36 +0530

    Remove unused env variable!

--- src/type-orm.config.ts
+++ src/type-orm.config.ts
@@ -8,8 +8,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: parseInt(process.env.RDS_PORT, 10) || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
-  password:
-    process.env.RDS_PASSWORD || process.env.LDB_PASSWORD || dbConfig.password,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
